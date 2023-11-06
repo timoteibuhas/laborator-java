dfad4f6cc921a1f8bfced5c28e32ffbd038c7890
Timotei Buhas <buhas.timoteipaul@student.uoradea.ro>
2023-11-06 18:53:23 +0200

    d

--- src/lab5/Cat.java
+++ src/lab5/Cat.java
@@ -8,6 +8,30 @@ public class Cat {
 
     private Integer age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     private Integer energyLevel;
 
     private Integer hungerLevel;
--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -1,8 +1,11 @@
 package lab5;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        /*
         Cat firstCat = new Cat("Tomita","Gri",5);
         Cat secondCat = new Cat("Miti","Negru",2);
 
@@ -10,6 +13,33 @@ public class Main {
         firstCat.feed();
         firstCat.sleep();
         firstCat.miau();
+
+        firstCat.getName();
+        firstCat.getColor();
+        firstCat.getAge();
+
+         */
+
+
+    Scanner input=new Scanner(System.in);
+        Employee emp = new Employee();
+        System.out.println("Introduceti numele:");
+        emp.setName(input.nextLine());
+
+        System.out.println("Introduceti emailul:");
+        emp.setEmail(input.nextLine());
+
+        System.out.println("Introduceti hour rate:");
+        emp.setHourRate(input.nextInt());
+
+        System.out.println("Introduceti capacity:");
+        emp.setCapacity(input.nextInt());
+
+        System.out.println("Introduceti free days:");
+        emp.setFreeDays(input.nextInt());
+        emp.calculateMonthlyIncome();
+
+
 
     }
 }
